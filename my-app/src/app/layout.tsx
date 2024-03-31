f9537401d6a11f14ecc563357befe0181853f49b
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-03-31 02:44:11 +0200

    add google analytics

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -17,6 +17,25 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				{/* Google Analytics */}
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-1BYGX7Q0R9"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-1BYGX7Q0R9');
+                        `,
+					}}
+				/>
+				{/* End Google Analytics */}
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
