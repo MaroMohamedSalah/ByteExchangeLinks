a838962db6b511ac37a8b6967bb6c84587e65792
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-04-16 01:10:10 +0200

    update pdfs

--- my-app/src/app/pdfs/page.tsx
+++ my-app/src/app/pdfs/page.tsx
@@ -1,13 +1,24 @@
+"use client";
 import Link from "next/link";
 import content from "../admin/content.json";
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 const Pdfs = () => {
+	const [content, setContent] = useState({ pdfs: [] });
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const contentFromLocalStorage = localStorage.getItem("content");
+			contentFromLocalStorage &&
+				setContent(JSON.parse(contentFromLocalStorage));
+		}
+	}, []);
 	return (
 		<div className="Pdfs">
 			<div className="container">
 				<div className="optionsContainer">
 					<div className="row w-100">
-						{content.pdfs.map((item, index) => {
+						{content.pdfs.map((item: any, index: number) => {
 							return (
 								<div key={index} className="col-12 col-md-6 py-3">
 									<Link href={`${item.link}`} passHref target="_blank">
