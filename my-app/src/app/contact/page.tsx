2b1170c5fc6a4a360b6bc966ab1cd2b543e401bd
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-04-16 01:07:46 +0200

    update contact

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import { Button } from "@mui/material";
 import Link from "next/link";
-import content from "../admin/content.json";
+import { useEffect, useState } from "react";
 
 const Contact = () => {
+	const [content, setContent] = useState({ contact: [] });
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const contentFromLocalStorage = localStorage.getItem("content");
+			contentFromLocalStorage &&
+				setContent(JSON.parse(contentFromLocalStorage));
+		}
+	}, []);
 	return (
 		<div className="Contact">
 			<div className="container">
 				<div className="optionsContainer">
 					<div className="row w-100">
-						{content.contact.map((item, index) => {
+						{content.contact.map((item: any, index: number) => {
 							return (
 								<div key={index} className="col-12 col-md-6 py-3">
 									<Link href={`${item.link}`} passHref target="_blank">
